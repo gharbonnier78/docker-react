f069ed908b08c79d6d4f8110d7d4465cb7a30dda
HARBONNIER Guillaume <g073038@mph.morpho.com>
2020-03-17 21:20:20 +0100

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+# Specify a base image
+FROM ubuntu as builder
+
+
+#COPY sources.list /etc/apt/sources.list
+RUN apt-get clean all
+RUN apt-get update
+
+#RUN apt-get install git-core curl build-essential openssl libssl-dev python
+
+RUN apt -y install nodejs
+RUN apt -y install npm
+RUN apt -y install wget curl
+
+RUN npm cache clean -f
+RUN npm install -g n
+RUN n stable
+
+WORKDIR /usr/app
+
+
+# Install some depenendencies
+COPY ./package.json ./
+RUN npm install
+COPY ./ ./
+
+RUN npm run build
+
+FROM nginx
+COPY --from=builder /usr/app/build /usr/share/nginx/html
---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          AV 5.0 is now ready ! By Edimia. It workssss !!! Dios mio ! Que has hecho ?
+        </p>
+        <p>
+          WARNING: no live and no post event analysis available. Login feature will be available in next version.
+        </p>
+        <p>
+          Enjoy!!! You are so nice. Life is fun !!!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
